6868b504f0cc31c1b1d579bff760440d851f4dd6
Álvaro Ramos <alvaroramos2222@hotmail.com>
2021-11-10 10:00:38 -0300

    att exercicio4

--- Fundamentos/bloco-4-introducao-a-java-scrip-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio4.js
+++ Fundamentos/bloco-4-introducao-a-java-scrip-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio4.js
@@ -1,16 +1,16 @@
 // 4- Depois, faça uma pirâmide com n asteriscos de base:
 
 let n = 5;
+let n1 = n;
 
-for(let i = 1; i <= n; i += 2){
-
+for(let i = 1; i <= n; i +=2){
     let aux = "";
-
-    for(let j = n; j > i; j-=2){
+    n1 -= 2
+    for(let k = 1; k <= n1; k +=2){
         aux += " ";
     }
-    for(let k = 0; k < i; k+=1){
-        aux +="*";
+    for(let j = 1; j <= i; j += 1){
+        aux += "*";
     }
     console.log(aux);
 }
