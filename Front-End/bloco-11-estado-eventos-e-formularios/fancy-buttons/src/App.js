8395a45c4e7270b2a485eb6fa785e0b7e357727a
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-01-26 12:12:52 -0300

    Exercicio de fixacao this, bind e constructor

--- Front-End/bloco-11-estado-eventos-e-formularios/fancy-buttons/src/App.js
+++ Front-End/bloco-11-estado-eventos-e-formularios/fancy-buttons/src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import './App.css';
 
-const leftButton = () => {
-    console.log('Clicou no botão esquerdo')
-}
-
-const middleButton = () => {
-    console.log('Clicou no botão do meio')
-}
-
-const rightButton = () => {
-    console.log('Clicou no botão direito')
-}
-
 class App extends React.Component {
+    constructor() {
+
+        super ()
+
+        this.leftButton = this.leftButton.bind(this);
+        this.middleButton = this.middleButton.bind(this);
+        this.rightButton = this.rightButton.bind(this);
+
+    }
+
+    leftButton = () => {
+        console.log(this);
+        console.log('Clicou no botão esquerdo')
+    }
+    
+    middleButton = () => {
+        console.log(this);
+        console.log('Clicou no botão do meio');
+    }
+    
+    rightButton = () => {
+        console.log(this);
+        console.log('Clicou no botão direito');
+    }
+
   render() {
     return (
       <div>
-        <button onClick={leftButton}>LeftButton</button>
-        <button onClick={middleButton}>MiddleButton</button>
-        <button onClick={rightButton}>RightButton</button>
+        <button onClick={this.leftButton}>LeftButton</button>
+        <button onClick={this.middleButton}>MiddleButton</button>
+        <button onClick={this.rightButton}>RightButton</button>
       </div>
     )
   }
